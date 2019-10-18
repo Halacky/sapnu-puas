5c9772adb7de4c66c10a111ce2d6f3a97a81508f
Z3R0ing <56577337+Z3R0ing@users.noreply.github.com>
2019-10-18 10:34:40 +0800

    checkpoint 1 day 2
    Добавили макет сценария, добавили inline кнопки, реализовали макет общения с базой данных

--- Program.cs
+++ Program.cs
@@ -1,112 +1,109 @@
-using System;
-using Telegram.Bot;
-namespace TestBot1
-{
-    class Program
-    {
-        static TelegramBotClient bot;
-        static void Main()
-        {
-            try
-            {
-                bot = new TelegramBotClient("914360727:AAHQOJKar_N__IViBNuNzJCru_uFUvGTac0");
-                Commands.bot = bot;
-                Console.WriteLine("Бот запущен!");
-                bot.OnMessage += Bot_OnMessage;
-                bot.StartReceiving();
-                Console.ReadKey();
-            }
-            catch
-            {
-                Console.WriteLine("ПРОИЗОШЛО ЧТО-ТО СТРАШНОЕ");
-                Console.ReadKey();
-            }
-
-        }
-
-        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
-        {
-            Console.WriteLine("Message! From: " + e.Message.From.FirstName + "\nText: " + e.Message.Text);
-            var message = e.Message;
-            var id = message.From.Id;
-
-            if (message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
-            {
-                string textMes = message.Text.ToLower();
-
-                if (textMes == "привет")
-                {
-                    Commands.hello(id);
-                }
-
-                if (textMes == "карта")
-                {
-                    Commands.map(id);
-                }
-                if (textMes == "сессия")
-                {
-                    Commands.setka(id);
-                }
-                if (textMes == "расписание")
-                {
-                    Commands.timetable(id);
-                }
-            }
-        }
-    }
-
-    static class Commands
-    {
-        public static TelegramBotClient bot;
-
-        public static void hello(Telegram.Bot.Types.ChatId id)
-        {
-            bot.SendTextMessageAsync(id, "Hello!");
-        }
-
-        public static void timetable(Telegram.Bot.Types.ChatId id)
-        {
-            bot.SendTextMessageAsync(id, "Сегодня у тебя нет пар!");
-        }
-
-        public static void map(Telegram.Bot.Types.ChatId id)
-        {
-            bot.SendPhotoAsync(id, "https://sun9-38.userapi.com/c853528/v853528470/1294ac/feNQQjMlNkc.jpg");
-        }
-        public static void setka(Telegram.Bot.Types.ChatId id)
-        {
-            /*
-             !!!!!!ФОРМАТ В БД ТИПА 01.01.2000!!!!!!
-             * twoDates - массив из БД с двумя датами
-             * сплитую по точке эти две даты
-             * отнимаю от дат текущую дату 
-             * получаю дни
-             * вывожу дни
-             */
-            String[] twoDates = new String[] {"12.12.2019","30.11.2019"};
-            var fromSetka = twoDates[0].Split('.');
-            DateTime dateFromSetka = new DateTime(int.Parse(fromSetka[2]), int.Parse(fromSetka[1]), int.Parse(fromSetka[0]));
-            var fromWeek = twoDates[1].Split('.');
-            DateTime dateFromWeek = new DateTime(int.Parse(fromWeek[2]), int.Parse(fromWeek[1]), int.Parse(fromWeek[0]));
-
-            var betweenSetka = dateFromSetka.Subtract(DateTime.Now);
-            var betweenWeek = dateFromWeek.Subtract(DateTime.Now);
-
-            bot.SendTextMessageAsync(id, $"Дней до начала сессии осталось: {betweenSetka.Days}. До начала зачетной недели: {betweenWeek.Days}");
-        }
-    }
-
     static class DataBase
     {
-        // var id_str = message.From.Id;
-        public static string[] userData(int id)
+        public enum dataTypes
         {
-            string str = "" + id + "~Иванов~Иван~Иванович~1998-09-03~null~АСУб-16-1~null~null";
-            return str.Split('~');
+            id, last_name, first_name, father_name, birthdate, number, ac_group, mail, vk_id
         }
-        public static void addBid(int id, string bidType)
+        //-------
+        // РџРѕР»СЊР·РѕРІР°С‚РµР»Рё
+        // var id_str = message.From.Id;
+        public static async Task<string[]> userData(int userId)
         {
+            //string str = "" + id + "~РРІР°РЅРѕРІ~РРІР°РЅ~РРІР°РЅРѕРІРёС‡~03.05.2020~null~РђРЎРЈР±-16-1~null~null";
+            string phpUrl = "";
+            return null;
+        }
+        public static async void addUserData(int userId, dataTypes data, string value)
+        {
+            string phpUrl = "";
+        }
+        public static async void addUser(int userId)
+        {
+            string phpUrl = "users/add_user.php";
+            string[] args = { "id=" + userId };
+            await phpResult(phpUrl, args);
+        }
+        public static async void regUser(int userId, string keyWord)
+        {
+            string phpUrl = "";
+        }
+        //---------
+        // РЎРїСЂР°РІРєРё
+        public static async void addBid(int userId, string bidType)
+        {
+            string[] args = { "id=" + userId , "bid_type=" + bidType};
+            string phpUrl = "bids/add_bid.php";
+            await phpResult(phpUrl, args);
+        }
+        // Р РµР·СѓР»СЊС‚Р°С‚ С‚РёРїР°
+        // string[] res = [ "РЎРїСЂР°РІРєР° СЃРѕС† Р·Р°С‰РёС‚Р° - РіРѕС‚РѕРІР°" , "РЎРїСЂР°РІРєР° РїРѕ РјРµСЃС‚Сѓ С‚СЂРµР±РѕРІР°РЅРёСЏ - РЅРµРіРѕС‚РѕРІР°" ];
+        public static async Task<string[]> getBidsStatus(int userId)
+        {
+            string phpUrl = "";
+            return null;
+        }
+        //----------
+        // РњРµСЂРѕРїСЂРёСЏС‚РёСЏ
+        // Р’РѕР·РІСЂР°С‰Р°РµС‚ РјР°СЃСЃРёРІ РЅР°Р·РІР°РЅРёР№ РјРµСЂРѕРїСЂРёСЏС‚РёР№
+        public static async Task<string[]> getEventsList()
+        {
+            string phpUrl = "";
+            return null;
+        }
+        public static async Task<string[]> getEventInfo(string eventName)
+        {
+            string phpUrl = "";
+            return null;
+        }
+        private static string dataTypeToText(dataTypes dT)
+        {
+            switch (dT)
+            {
+                case dataTypes.id:
+                    return "id";
+                case dataTypes.last_name:
+                    return "last_name";
+                case dataTypes.first_name:
+                    return "first_name";
+                case dataTypes.father_name:
+                    return "father_name";
+                case dataTypes.birthdate:
+                    return "birthdate";
+                case dataTypes.number:
+                    return "number";
+                case dataTypes.ac_group:
+                    return "ac_group";
+                case dataTypes.mail:
+                    return "mail";
+                case dataTypes.vk_id:
+                    return "vk_id";
+                default:
+                    return null;
+            }
+        }
 
+        private static async Task<string> phpResult(string phpUrl, string[] args)
+        {
+            // build request
+            string url = "https://istu-bot.000webhostapp.com/" + phpUrl + "?";
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                url += args[i] + "&";
+            }
+            url += args[args.Length - 1];
+            //--------------
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = await request.GetResponseAsync();
+            string result = null;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            response.Close();
+            return result;
         }
     }
 }
